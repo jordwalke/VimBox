a8a86d3df8054c06d50a0da58a365b66e5967563
james2doyle <james2doyle@gmail.com>
2015-04-30 12:23:15 -0400

    add install font script

---
+++ install-font.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+PWD=$(pwd)
+
+read -n 1 -r -s -p "Click \"Install\" when prompted. Press any key to continue.."
+
+open "$PWD/dotVim/Fonts/InconsolataDz/Inconsolata-dz for Powerline.otf"
