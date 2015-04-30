f7ca58ff11c1ae6a36d312c95dbdcde8b28492ef
james2doyle <james2doyle@gmail.com>
2015-04-30 12:28:05 -0400

    fix path errors! whoops!

--- symlink-rcs.sh
+++ symlink-rcs.sh
@@ -3,22 +3,22 @@
 PWD=$(pwd)
 
 if [[ ! -d "$HOME/.vim" && ! -L "$HOME/.vim" ]]; then
-  echo -e "Moving $PWD/VimBox/dotVim to ~/.vim"
-  ln -s "$PWD/VimBox/dotVim" ~/.vim
+  echo -e "Moving $PWD/dotVim to ~/.vim"
+  ln -s "$PWD/dotVim" ~/.vim
 else
   echo -e "$HOME/.vim was already moved, or already exists"
 fi
 
 if [[ ! -f "$HOME/.vimrc" && ! -L "$HOME/.vimrc" ]]; then
-  echo -e "Moving $PWD/VimBox/dotVimRc to ~/.vimrc"
-  ln -s "$PWD/VimBox/dotVim" ~/.vimrc
+  echo -e "Moving $PWD/dotVimRc to ~/.vimrc"
+  ln -s "$PWD/dotVimRc" ~/.vimrc
 else
   echo -e "$HOME/.vimrc was already moved, or already exists"
 fi
 
 if [[ ! -f "$HOME/.gvimrc" && ! -L "$HOME/.gvimrc" ]]; then
-  echo -e "Moving $PWD/VimBox/dotGVimRc to ~/.gvimrc"
-  ln -s "$PWD/VimBox/dotVim" ~/.gvimrc
+  echo -e "Moving $PWD/dotGVimRc to ~/.gvimrc"
+  ln -s "$PWD/dotGVimRc" ~/.gvimrc
 else
   echo -e "$HOME/.gvimrc was already moved, or already exists"
 fi
