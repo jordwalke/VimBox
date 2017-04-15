40d0d85a0111d4687699340fba3eef402e05a880
Jordan Walke <jordojw@gmail.com>
2017-04-14 21:56:16 -0700

    Add Iosevka font enabled by default, and installed automatically

--- install-script.sh
+++ install-script.sh
@@ -67,14 +67,29 @@ if [[ -f "$PWD/dotVimRc" && ! -L "$PWD/dotVimRc" ]]; then
     echo -e "Not linking from $HOME/.gvimrc - it was already moved, or already existed"
   fi
 
-  FONT_SOURCE="$PWD/dotVim/Fonts/InconsolataDz/Inconsolata-dz for Powerline.otf"
-  FONT_DEST="$HOME/Library/Fonts/Inconsolata-dz for Powerline.otf"
-
+  FONT_SOURCE="$PWD/dotVim/Fonts/Iosevka/iosevka-regular.ttf"
+  FONT_DEST="$HOME/Library/Fonts/iosevka-regular.ttf"
   if [[ ! -f "$FONT_DEST" ]]; then
     echo -e "Installing font $FONT_SOURCE into $FONT_DEST"
     cp "$FONT_SOURCE" "$FONT_DEST"
   else
-    echo -e "The font was already installed"
+    echo -e "The font $FONT_DEST was already installed"
+  fi
+  FONT_SOURCE="$PWD/dotVim/Fonts/Iosevka/iosevka-bold.ttf"
+  FONT_DEST="$HOME/Library/Fonts/iosevka-bold.ttf"
+  if [[ ! -f "$FONT_DEST" ]]; then
+    echo -e "Installing font $FONT_SOURCE into $FONT_DEST"
+    cp "$FONT_SOURCE" "$FONT_DEST"
+  else
+    echo -e "The font $FONT_DEST was already installed"
+  fi
+  FONT_SOURCE="$PWD/dotVim/Fonts/Iosevka/iosevka-italic.ttf"
+  FONT_DEST="$HOME/Library/Fonts/iosevka-italic.ttf"
+  if [[ ! -f "$FONT_DEST" ]]; then
+    echo -e "Installing font $FONT_SOURCE into $FONT_DEST"
+    cp "$FONT_SOURCE" "$FONT_DEST"
+  else
+    echo -e "The font $FONT_DEST was already installed"
   fi
 else
   echo -e "It seems you are not running the installer from within the VimBox root"
